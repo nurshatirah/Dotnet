85cfe256d25fdc67eda9a4801e0d425114330230
G-40 <G-40@LENOVO-PC>
2018-10-23 16:21:37 +0800

    this is my first commit

---
+++ WebApplication7/WebApplication7/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication7.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" style="height: 22px"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="Enter a number"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication7/WebApplication7/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication7
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+                string tString = TextBox1.Text;
+                if (tString.Trim() == "") return;
+                for (int i = 0; i < tString.Length; i++)
+                {
+                    if (!char.IsNumber(tString[i]))
+                    {
+                        Label1.Text = "Please enter a valid number";
+                        TextBox1.Text = "";
+                        return;
+                    }
+
+                }
+                //If it get's here it's a valid number
+            }
+        }
+    }
